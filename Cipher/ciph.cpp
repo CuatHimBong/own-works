8720bdd8f32076ac2e8918f18f0d341220b4ec5c
CuatHimBong <egvika20051@gmail.com>
2025-05-03 18:41:00 +0300

    Fixed whitespaces

--- Cipher/ciph.cpp
+++ Cipher/ciph.cpp
@@ -2,21 +2,21 @@
 #include <string>
 
 int main() {
-	char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	std::string word = "";
+  char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+  std::string word = "";
 
-	std::cin >> word;
+  std::cin >> word;
 
-	unsigned int size = word.size();
-	char* wordp = new char[size];
+  unsigned int size = word.size();
+  char* wordp = new char[size];
 
-	word.copy(wordp, size, 0);
+  word.copy(wordp, size, 0);
 
-	for (int i = 0; i < size; i++) {
-		std::cout << wordp[i];
-	}
-	std::cout << "\n";
+  for (int i = 0; i < size; i++) {
+    std::cout << wordp[i];
+  }
+  std::cout << "\n";
 
-	delete[] wordp;
+  delete[] wordp;
 }
 
